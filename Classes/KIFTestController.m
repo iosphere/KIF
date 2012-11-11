92c6b0cbfcf6cabfe200b3c2ac740e3e7212811c
Felix Schneider <schneider@iosphere.de>
2012-11-11 03:56:49 +0100

    adds NSTemporaryDirectory log path prefix

--- Classes/KIFTestController.m
+++ Classes/KIFTestController.m
@@ -483,7 +483,7 @@ static void releaseInstance()
 {
     static NSFileHandle *fileHandle = nil;
     if (!fileHandle) {
-        NSString *logsDirectory = [[[NSProcessInfo processInfo] environment] objectForKey:@"KIF_SCREENSHOTS"];
+        NSString *logsDirectory = [NSTemporaryDirectory() stringByAppendingPathComponent:[[[NSProcessInfo processInfo] environment] objectForKey:@"KIF_SCREENSHOTS"]];
         [logsDirectory stringByDeletingLastPathComponent];
         
         NSString *logFilePath = [logsDirectory stringByAppendingPathComponent:@"current.log"];
